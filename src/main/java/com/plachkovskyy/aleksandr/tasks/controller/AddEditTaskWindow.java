115d40f27a441532186ea546471403960bdc45e3
alex-sumy-ua <plachkovskyy@gmail.com>
2019-04-15 00:51:37 +0300

    20190415-01

--- src/main/java/com/plachkovskyy/aleksandr/tasks/controller/AddEditTaskWindow.java
+++ src/main/java/com/plachkovskyy/aleksandr/tasks/controller/AddEditTaskWindow.java
@@ -1,13 +1,16 @@
 package com.plachkovskyy.aleksandr.tasks.controller;
 
 import java.net.URL;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.ResourceBundle;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.RadioButton;
+import javafx.scene.control.*;
 import org.apache.log4j.Logger;
 
 public class AddEditTaskWindow {
@@ -23,10 +26,70 @@ public class AddEditTaskWindow {
     private URL location;
 
     @FXML
+    private DatePicker fromDatePicker;
+
+    @FXML
+    private TextField atMinutes;
+
+    @FXML
+    private TextField fromMinutes;
+
+    @FXML
+    private TextField atHours;
+
+    @FXML
+    private DatePicker toDatePicker;
+
+    @FXML
+    private Slider fromHoursSlider;
+
+    @FXML
+    private TextField intervalMinutes;
+
+    @FXML
+    private Slider fromMinutesSlider;
+
+    @FXML
+    private TextField fromHours;
+
+    @FXML
+    private Slider intervalHoursSlider;
+
+    @FXML
+    private Slider atHoursSlider;
+
+    @FXML
     private Button cancelButton;
 
     @FXML
     private DatePicker atDatePicker;
+
+    @FXML
+    private Slider toMinutesSlider;
+
+    @FXML
+    private TextField intervalDays;
+
+    @FXML
+    private Slider toHoursSlider;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private Slider intervalMinutesSlider;
+
+    @FXML
+    private Slider atMinutesSlider;
+
+    @FXML
+    private TextField intervalHours;
+
+    @FXML
+    private TextField toMinutes;
+
+    @FXML
+    private TextField toHours;
 
     @FXML
     private RadioButton nonRepeatedRadioButton;
@@ -35,14 +98,23 @@ public class AddEditTaskWindow {
     private RadioButton repeatedRadioButton;
 
     @FXML
-    private Button addButton;
+    private Slider intervalDaysSlider;
+
+    @FXML
+    private CheckBox activeCheckBox;
 
     @FXML
     void onAddButtonPressed(ActionEvent event) {
         //
-        addButton.getScene().getWindow().hide();
+        saveButton.getScene().getWindow().hide();
 
     }
+
+    @FXML
+    void onActiveCheckBoxChecked (ActionEvent event) {
+
+    }
+
 
     @FXML
     void onCancelButtonPressed(ActionEvent event) {
@@ -51,16 +123,165 @@ public class AddEditTaskWindow {
 
     @FXML
     void onNonRepeatedRadioButton(ActionEvent event) {
+        nonRepeatedRadioButton.setSelected(true);
+        repeatedRadioButton.setSelected(!nonRepeatedRadioButton.selectedProperty().getValue());
+        nonRepeatedBlockSetEnabled(true);
+        repeatedBlockSetEnabled(false);
 
     }
 
     @FXML
     void onRepeatedRadioButton(ActionEvent event) {
+        repeatedRadioButton.setSelected(true);
+        nonRepeatedRadioButton.setSelected(!repeatedRadioButton.selectedProperty().getValue());
+        repeatedBlockSetEnabled(true);
+        nonRepeatedBlockSetEnabled(false);
 
+    }
+
+    void nonRepeatedBlockSetEnabled(boolean enable) {
+        fromDatePicker.setDisable(enable);
+        fromHoursSlider.setDisable(enable);
+        fromMinutesSlider.setDisable(enable);
+        toDatePicker.setDisable(enable);
+        toHoursSlider.setDisable(enable);
+        toMinutesSlider.setDisable(enable);
+        fromHours.setDisable(enable);
+        fromMinutes.setDisable(enable);
+        toHours.setDisable(enable);
+        toMinutes.setDisable(enable);
+        intervalDays.setDisable(enable);
+        intervalHours.setDisable(enable);
+        intervalMinutes.setDisable(enable);
+        intervalDaysSlider.setDisable(enable);
+        intervalHoursSlider.setDisable(enable);
+        intervalMinutesSlider.setDisable(enable);
+        atDatePicker.setDisable(!enable);
+        atHoursSlider.setDisable(!enable);
+        atMinutesSlider.setDisable(!enable);
+        atHours.setDisable(!enable);
+        atMinutes.setDisable(!enable);
+    }
+
+    void repeatedBlockSetEnabled(boolean enable) {
+        fromDatePicker.setDisable(!enable);
+        fromHoursSlider.setDisable(!enable);
+        fromMinutesSlider.setDisable(!enable);
+        toDatePicker.setDisable(!enable);
+        toHoursSlider.setDisable(!enable);
+        toMinutesSlider.setDisable(!enable);
+        fromHours.setDisable(!enable);
+        fromMinutes.setDisable(!enable);
+        toHours.setDisable(!enable);
+        toMinutes.setDisable(!enable);
+        intervalDays.setDisable(!enable);
+        intervalHours.setDisable(!enable);
+        intervalMinutes.setDisable(!enable);
+        intervalDaysSlider.setDisable(!enable);
+        intervalHoursSlider.setDisable(!enable);
+        intervalMinutesSlider.setDisable(!enable);
+        atDatePicker.setDisable(enable);
+        atHoursSlider.setDisable(enable);
+        atMinutesSlider.setDisable(enable);
+        atHours.setDisable(enable);
+        atMinutes.setDisable(enable);
     }
 
     @FXML
     void initialize() {
+        atHours.setText("0");
+        atMinutes.setText("0");
+        fromHours.setText("0");
+        fromMinutes.setText("0");
+        toHours.setText("0");
+        toMinutes.setText("0");
+        intervalDays.setText("0");
+        intervalHours.setText("0");
+        intervalMinutes.setText("0");
+        atDatePicker.setValue(LocalDate.now());
+        fromDatePicker.setValue(LocalDate.now());
+        toDatePicker.setValue(LocalDate.now().plusDays(7));
+        nonRepeatedRadioButton.setSelected(true);
+        nonRepeatedBlockSetEnabled(true);
+        repeatedBlockSetEnabled(false);
+        //atHours.textProperty().bindBidirectional(atHoursSlider.valueProperty(), NumberFormat.getNumberInstance());
+
+        // atHoursSlider Listener
+        atHoursSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
+                //int current = new_val.intValue();
+                atHours.setText(String.format("%.0f", new_val));
+            }
+        });
+
+        // atMinutesSlider Listener
+        atMinutesSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
+                //int current = new_val.intValue();
+                atMinutes.setText(String.format("%.0f", new_val));
+            }
+        });
+
+        // fromHoursSlider Listener
+        fromHoursSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
+                //int current = new_val.intValue();
+                fromHours.setText(String.format("%.0f", new_val));
+            }
+        });
+
+        // fromMinutesSlider Listener
+        fromMinutesSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
+                //int current = new_val.intValue();
+                fromMinutes.setText(String.format("%.0f", new_val));
+            }
+        });
+
+        // toHoursSlider Listener
+        toHoursSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
+                //int current = new_val.intValue();
+                toHours.setText(String.format("%.0f", new_val));
+            }
+        });
+
+        // toMinutesSlider Listener
+        toMinutesSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
+                //int current = new_val.intValue();
+                toMinutes.setText(String.format("%.0f", new_val));
+            }
+        });
+
+        // intervalDaysSlider Listener
+        intervalDaysSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
+                //int current = new_val.intValue();
+                intervalDays.setText(String.format("%.0f", new_val));
+            }
+        });
+
+        // intervalHoursSlider Listener
+        intervalHoursSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
+                //int current = new_val.intValue();
+                intervalHours.setText(String.format("%.0f", new_val));
+            }
+        });
+
+        // intervalMinutesSlider Listener
+        intervalMinutesSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
+                //int current = new_val.intValue();
+                intervalMinutes.setText(String.format("%.0f", new_val));
+            }
+        });
+
+
+
 
     }
+
+
 }
