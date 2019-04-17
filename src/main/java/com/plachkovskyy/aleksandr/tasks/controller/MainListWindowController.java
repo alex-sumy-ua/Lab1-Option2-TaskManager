d59eb0f872c693e89cc749446d5eb5f8ae34981e
alex-sumy-ua <plachkovskyy@gmail.com>
2019-04-17 14:05:47 +0300

    Add files via upload

---
+++ src/main/java/com/plachkovskyy/aleksandr/tasks/controller/MainListWindowController.java
@@ -0,0 +1,125 @@
+package com.plachkovskyy.aleksandr.tasks.controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import org.apache.log4j.Logger;
+
+public class MainListWindowController {
+
+    final static Logger logger = Logger.getLogger(StartController.class);
+    //        logger.info("Logger testing - info");
+    //        logger.error("Logger testing - error");
+
+    private String mode;
+
+    public String getMode() {return mode; }
+    public void setMode(String mode) {
+        this.mode = mode;
+        modeLabel.setText("Using " + mode + " list");
+    }
+
+//    public MainController(String mode) {
+//        this.mode = mode;
+//    }
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button calendarButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private Button detailsButton;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Label modeLabel;
+
+    @FXML
+    void onCalendarButtonPressed(ActionEvent actionEvent) {
+        try {
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("/view/fxml/calendarWindow.fxml"));
+            stage.setTitle("TaskManager: calendar");
+            stage.setMinHeight(400);
+            stage.setMinWidth(600);
+            stage.setResizable(false);
+            stage.setScene(new Scene(root));
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(((Node) actionEvent.getSource()).getScene().getWindow());
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void onAddButtonPressed(ActionEvent actionEvent) {
+        try {
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("/view/fxml/addEditTaskWindow.fxml"));
+            stage.setTitle("TaskManager: add new task");
+            stage.setMinHeight(400);
+            stage.setMinWidth(600);
+            stage.setResizable(false);
+            stage.setScene(new Scene(root));
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(((Node) actionEvent.getSource()).getScene().getWindow());
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void onDetailsButtonPressed(ActionEvent actionEvent) {
+        try {
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("/view/fxml/addEditTaskWindow.fxml"));
+            stage.setTitle("TaskManager: view/edit current task");
+            stage.setMinHeight(400);
+            stage.setMinWidth(600);
+            stage.setResizable(false);
+            stage.setScene(new Scene(root));
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(((Node) actionEvent.getSource()).getScene().getWindow());
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void onExitButtonPressed(ActionEvent event) {
+        logger.info("Logger: application finished at MainWindow.exitButton");
+        System.exit(0);
+    }
+
+    @FXML
+    void initialize() {
+
+
+        logger.info("Logger: MainWindow started. Using " + getMode() + " list.");
+
+
+    }
+}
